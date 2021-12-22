6d8ccc94cc2f7c691dbd9279f30fbb6d82b30d10
AneeshCodes <74843895+AneeshCodes@users.noreply.github.com>
2021-12-22 10:04:08 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
-function preload(){
-  video  = createVideo('video.mp4')
-}
+find = ''
 
 function setup(){
   canvas = createCanvas(600, 500)
   canvas.center()
-  
+  video  = createCapture()
 }
 
 function draw(){
+  image(video, 0, 0, 600, 500)
+  video.hide()
+}
 
+function start(){
+  objectDetected = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById('status').innerHTML = 'Detecting Objects';
+  video.play()
+  find = document.getElementById('object_name').value
+}
+
+function modelLoaded() {
+  console.log('model is loaded');
+  status = true;
 }
